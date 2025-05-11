464bb8b4c99762c0dd0be91392513e2563f2d42c
jobeljohny <jobeljohny@gmail.com>
2025-05-11 13:23:54 +0530

    added nav on payment added

--- Screens/AddPayment.tsx
+++ Screens/AddPayment.tsx
@@ -4,12 +4,24 @@ import PageHeader from "../Components/PageHeader";
 import PaymentInfoForm from "../Components/PaymentInfoForm";
 import { IPaymentInfo } from "../Interfaces/payment";
 import { addExpense, db } from "../Services/DbManager";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../App";
+
+type AddPaymentNavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  "AddPayment"
+>;
 
 export default function AddPayment() {
+  const navigation = useNavigation<AddPaymentNavigationProp>();
+
   const handleFormSubmit = async (values: IPaymentInfo) => {
     try {
       const record = await addExpense(db, values);
       console.log("Expense recorded successfully:", record);
+      // Navigate back to ViewPayments after successful submission
+      navigation.navigate("ViewPayments");
     } catch (error) {
       console.error("Error while adding expense:", error);
     }
